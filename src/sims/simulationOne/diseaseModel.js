50d5781e5d902871d226a31f16d8ece4c6f4d0d7
cee1904 <itscece890@gmail.com>
2025-02-27 13:39:06 +0000

    notes-cik

--- src/sims/simulationOne/diseaseModel.js
+++ src/sims/simulationOne/diseaseModel.js
@@ -23,9 +23,10 @@ and not interacting with others in each round.
  * 
  * What we are simulating:Disease Spread Model
  * 
- * What elements we have to add:Q
+ * What elements we have to add:  Death Rate
  * 
- * In plain language, what our model does: 
+ * In plain language, what our model does
+ *: Our model is going to track the death rate of the infected.
  * 
  */
 
